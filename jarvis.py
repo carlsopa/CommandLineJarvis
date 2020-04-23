814deb90868583cc66c72ca2186054cbeb91dcf0
Paul R. Carlson <carlsonpar@gmail.com>
2020-04-22 20:14:30 -0400

    updated code to clean up if else statements

--- jarvis.py
+++ jarvis.py
@@ -1,20 +1,48 @@
 import click 
 import os
 import shutil
+from collections import OrderedDict
 #global variables
 CurrentDirectory = os.getcwd()
 
-def CreateController(prompt):
+def CreateController(self,prompt):
   objectItem = ''
   actionItem = ''
-  action= ['create','new','destroy','remove','delete','recycle','move','rename','append']
-  object = ['file','document','directory','folder']
+  ActionType = OrderedDict({
+    "create": "create",
+    "new": "create",
+    "delete": "delete",
+    "destroy": "delete",
+    "remove": "delete",
+    "recycle":"delete",
+    "move":"move",
+    "rename":"rename",
+    "change":"rename" 
+  })
+  ObjectType = OrderedDict({
+    "file":"file",
+    "document":"file",
+    "directory":"directory",
+    "folder":"directory"
+  })
+#  action= ['create','new','destroy','remove','delete','recycle','move','rename','change','append']
+#  object = ['file','document','directory','folder']
   for i in prompt:
-    if i in action:
-      actionItem = str(action.index(i))
-    if i in object:
-      objectItem = str(object.index(i))
-  return [actionItem,objectItem]
+    #print(i)
+    if i in ActionType:
+      ActionItem = ActionType.get(i).capitalize()
+    if i in ObjectType:
+      ObjectItem = ObjectType.get(i).capitalize()
+  print(ActionItem + ObjectItem)
+  getattr(self,ActionItem + ObjectItem)()
+#      ActionItem = str(action.index(i))
+#    if i in Object.keys(ObjectType):
+#      objectItem = str(object.index(i))
+#  return [actionItem,objectItem]
+
+#selectedAction = action[actionItem]
+#selectedObject = object[objectItem]
+#print(selectedAction + selectedObject)
 
 def PerformAction(controller):
   if controller[0] =='' or controller[1] == '':
@@ -35,7 +63,7 @@ def PerformAction(controller):
         MoveFile()
       else:
         MoveDirectory()
-    elif int(controller[0]) == 7:
+    elif int(controller[0]) in range(7,9):
       if int(controller[1]) in range(0,2):
         ReNameFile()
       else:
@@ -47,7 +75,7 @@ def PerformAction(controller):
 #This will allow you to create a new file in the current directory
 def CreateFile():
   print("create a new file")
-  prompt = input("what is the name of your file(include extension): ")
+  prompt = input("what is the name of your file (include extension): ")
   file = open(prompt,"w+")
   file.close()
   
@@ -57,7 +85,7 @@ def CreateFile():
 def DeleteFile():
   print("delete file")
   print(os.listdir("."))
-  prompt = input("select a file to be delted and type it in: ")
+  prompt = input("select a file to be deleted and type it in: ")
   os.remove(prompt)
 
 #CreateDirectory()
@@ -68,7 +96,7 @@ def CreateDirectory():
   os.makedirs(os.path.join(CurrentDirectory,prompt))
 
 #DeleteDirectory()
-#Allows for you delete a given directory
+#Allows for you to delete a given directory
 #It will prompt with a list of directories for you to choose from.  Then it will ask if you truly want to delete that directory.
 def DeleteDirectory():
   print("Delete Directory")
@@ -87,7 +115,7 @@ def DeleteDirectory():
     elif prompt.capitalize().find("N") == 0:
       print("don't delete me")
     else:
-      print("my appologies, I did not understand your response")
+      print("my apologies, I did not understand your response")
   else:
     prompt = input("are you sure you want to delete the folder "+DirectoryPrompt+"?  This will delete it and all its contents.  This action can not be undone (yes/no): ")
     if prompt.capitalize().find("Y") == 0:
@@ -95,11 +123,11 @@ def DeleteDirectory():
         shutil.rmtree(DeletionItem)
       except OSError as error:
         print(error)
-        print("Directory '%s' can not be removed" %DeletionItem)
+        print("Directory '%s' cannot be removed" %DeletionItem)
     elif prompt.capitalize().find("N") == 0:
       print("don't delete me")
     else:
-      print("my appologies, I did not understand your response")
+      print("my apologies, I did not understand your response")
 
 #ReName()
 #Allows you to rename any given file
@@ -107,21 +135,19 @@ def ReNameFile():
   print("file rename")
   sep = "."
   print(os.listdir(CurrentDirectory))
-  NewNamePrompt = ''
   OriginalPrompt = input("please choose the file that you wish to rename: ")
   if OriginalPrompt in os.listdir(CurrentDirectory):
     FileExtension = OriginalPrompt.split(sep)[1]
-    NewNamePrompt = input("what is the new name for your file(DO NOT include .extension): ")
+    NewNamePrompt = input("what is the new name for your file (DO NOT include .extension): ")
     try:
       os.rename(OriginalPrompt,NewNamePrompt+"."+FileExtension)
     except OSError as error:
       print(error)
       print("'%s' file can not be created" %NewNamePrompt)
   else:
-    print(NewNamePrompt)
     NotFoundPrompt = input("it appears that your file was not found, would you like to create it instead(yes/no): ")
     if NotFoundPrompt.capitalize().find("Y") == 0:
-      open(NewNamePrompt, "w+")
+      open(OriginalPrompt, "w+")
     elif NotFoundPrompt.capitalize().find("N") == 0:
       print("ok, maybe next time")
     else:
@@ -131,15 +157,12 @@ def ReNameDirectory():
   print("directory rename")
   print(os.listdir(CurrentDirectory))
   OriginalPrompt = input("please choose a folder to rename: ")
-  #if os.listdir(OriginalPrompt) == []:
   NewNamePrompt = input("please enter a new name for your folder: ")
   try:
     os.rename(OriginalPrompt,NewNamePrompt)
   except OSError as error:
     print(error)
     print("we had an issue updating {} to {}." .format(OriginalPrompt, NewNamePrompt))
-  #else:
-
 
 #MoveFile()
 #Allows you to easily move your file from one directory to another
@@ -162,8 +185,8 @@ def Jarvis():
     if prompt == 'exit()':
       exit()
     promptlist = list(prompt.split(" "))
-    #CreateController(promptlist)
-    PerformAction(CreateController(promptlist))
+    CreateController(promptlist)
+    #PerformAction(CreateController(promptlist))
 
 
 if __name__ == '__main__':
